f5669b55121b9974a962e4791db86053824c26cc
Matteo Gigot <mgigot@fu-r6-p1.s19.be>
2024-02-13 11:14:08 +0100

    new signal and exit_code

--- src/signal.c
+++ src/signal.c
@@ -1,30 +1,39 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rheck <rheck@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mgigot <mgigot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 12:16:50 by rheck             #+#    #+#             */
-/*   Updated: 2024/02/06 11:24:10 by rheck            ###   ########.fr       */
+/*   Updated: 2024/02/13 11:03:05 by mgigot           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../inc/mini.h"
 
 int event(void)
 {
-    my_global.last_err_code = 130;
-    return(my_global.last_err_code);
+    return(0);
 }
-void    signal_handler(int sig)
+void	signal_handler(int sig)
 {
-	(void) sig;
-
-	rl_replace_line("\n", 0);
-    rl_redisplay();
+	if (!my_global.in_heredoc)
+		ft_putstr_fd("\n", 2);
+	if (my_global.in_cmd)
+	{
+		my_global.stop_heredoc = 1;
+		rl_replace_line("", 0);
+		rl_redisplay();
+		rl_done = 1;
+		return ;
+	}
+	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
+	(void) sig;
 }
+
 void    init_signal(void)
 {
     rl_event_hook = event;
